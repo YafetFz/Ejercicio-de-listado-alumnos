e8115f7bef62596ea28a7189b7112bb3a858d6df
YafetFz <ayafetfz@gmail.com>
2026-02-19 17:28:49 -0600

    Se creo la clase alumnos y se introdujo la lista

--- Yafet_Flores_Listado_Alumnos/Alumno.cs
+++ Yafet_Flores_Listado_Alumnos/Alumno.cs
@@ -6,6 +6,46 @@ namespace Yafet_Flores_Listado_Alumnos
 {
     public class Alumno
     {
+        //Atributos
+        private string nombre;
+        private string apellido;
+        private string fechaNacimiento;
+        private int nota;
 
+        //Encapsulamiento
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Apellido
+        {
+            get{ return apellido; }
+            set { apellido = value; }
+        }
+
+        public string FechaNacimiento
+        {
+            get { return fechaNacimiento; }
+            set { fechaNacimiento = value; }
+        }
+
+        public int Nota
+        {
+            get { return nota; }
+            set { nota = value; }
+        }
+
+        //Constructor
+        public Alumno(string nombre, string apellido, string fechaNacimiento, int nota)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.fechaNacimiento=fechaNacimiento;
+            this.nota = nota;
+        } 
+            
+        
     }
 }
--- Yafet_Flores_Listado_Alumnos/Program.cs
+++ Yafet_Flores_Listado_Alumnos/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Yafet_Flores_Listado_Alumnos;
+
+Console.WriteLine("Yafet Alessandro Flores Zavala");
+List<Alumno> alumnos = new List<Alumno>()
+{
+    new Alumno("Juan", "Perez", "2004-05-10", 85),
+    new Alumno("Maria", "Lopez", "2003-08-15", 90),
+    new Alumno("Carlos", "Perez", "2004-05-10", 70),
+    new Alumno("Ana", "Martinez", "2005-02-20", 95),
+    new Alumno("Luis", "Gomez", "2003-11-30", 60),
+    new Alumno("Sofia", "Hernandez", "2004-01-12", 88),
+    new Alumno("Pedro", "Ramirez", "2005-07-07", 76),
+    new Alumno("Elena", "Perez", "2003-08-15", 92)
+};
